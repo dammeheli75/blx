73ec05a69f6a58f01bcca5353e013bceb3d88323
dammeheli75 <dammeheli75@me.com>
2013-08-26 02:52:10 +0700

    Brand new map and geolocation feature

--- public_html/common/js/home.js
+++ public_html/common/js/home.js
@@ -1,67 +1,125 @@
 $(document).ready(function () {
-
     $('.carousel').carousel({
         interval: 5000
     });
+});
 
-    var fillingPlaceTab = $('#fillingPlaceTab');
-
-    fillingPlaceTab.find('a').click(function (e) {
-        e.preventDefault();
-        $(this).tab('show');
+function initialize() {
+    var collaboratorSellector = $("tbody tr");
+    var locations = [], markers = [], infos = [];
+    collaboratorSellector.each(function (index, item) {
+        var longitude = $(item).attr("data-longitude");
+        var latitude = $(item).attr("data-latitude");
+        var title = $(item).attr("data-title");
+        var info = $(item).attr("data-info");
+        var position = new google.maps.LatLng(longitude, latitude);
+        locations.push(position);
+        markers.push(new google.maps.Marker({
+            position: position,
+            animation: google.maps.Animation.DROP,
+            title: title
+        }));
+        infos.push(new google.maps.InfoWindow({
+            content: info
+        }));
     });
 
-    var map;
-
-    var vietDungLocation = new google.maps.LatLng(21.005321, 105.847150);
-    var manhDuyLocation = new google.maps.LatLng(21.006347, 105.846906);
-    var camKhangLocation = new google.maps.LatLng(21.004975, 105.847043);
-
-    map = new google.maps.Map(document.getElementById('map-canvas'), {
+    var mapOptions = {
         zoom: 18,
-        center: vietDungLocation,
         mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    if (locations.length > 0) {
+        mapOptions.center = locations[0];
+    }
+    // Enable the visual refresh
+    google.maps.visualRefresh = true;
+    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+    for (var i = 0; i < markers.length; i++) {
+        (function () {
+            var marker = markers[i];
+            var info = infos[i];
+            marker.setMap(map);
+            google.maps.event.addListener(marker, 'click', function () {
+                info.open(map, marker);
+            });
+        })();
+    }
+
+    collaboratorSellector.each(function (index, item) {
+        $(item).click(function () {
+            map.panTo(locations[index]);
+        });
     });
 
-    new google.maps.Marker({
-        position: vietDungLocation,
-        map: map,
-        draggable: false,
-        animation: google.maps.Animation.DROP
-    });
-
-    new google.maps.Marker({
-        position: manhDuyLocation,
-        map: map,
-        draggable: false,
-        animation: google.maps.Animation.DROP
-    });
-
-    new google.maps.Marker({
-        position: camKhangLocation,
-        map: map,
-        draggable: false,
-        animation: google.maps.Animation.DROP
-    });
-
-    fillingPlaceTab.find('a').on('shown', function (e) {
-        var tabLocation = $(e.target).attr('href');
-        var location;
-
-        switch (tabLocation) {
-            case '#collaborator-viet-dung':
-                location = vietDungLocation;
-                break;
-            case '#collaborator-manh-duy':
-                location = manhDuyLocation;
-                break;
-            case '#collaborator-cam-khang':
-                location = camKhangLocation;
-                break;
-            default:
-                break;
+    $("#findNearestLocation").click(function () {
+        var browserSupportFlag = Boolean();
+        // Try W3C Geolocation (Preferred)
+        if (navigator.geolocation) {
+            browserSupportFlag = true;
+            navigator.geolocation.getCurrentPosition(function (position) {
+                var geoLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+                // Do more
+                var service = new google.maps.DistanceMatrixService();
+                service.getDistanceMatrix(
+                    {
+                        origins: [geoLocation],
+                        destinations: locations,
+                        travelMode: google.maps.TravelMode.DRIVING,
+                        unitSystem: google.maps.UnitSystem.METRIC,
+                        durationInTraffic: true,
+                        avoidHighways: false,
+                        avoidTolls: false
+                    }, function (response, status) {
+                        // See Parsing the Results for
+                        // the basics of a callback function.
+                        var durations = [], i, minDurationIndex = null;
+                        for (i = 0; i < response.rows[0].elements.length; i++) {
+                            durations.push(response.rows[0].elements[i].duration.value);
+                        }
+                        var minDuration = Math.min.apply(Math, durations);
+                        for (i = 0; i < durations.length; i++) {
+                            if (durations[i] == minDuration) {
+                                minDurationIndex = i;
+                                break;
+                            }
+                        }
+                        if (minDurationIndex != null) {
+                            collaboratorSellector.each(function (index, item) {
+                                if (index == minDurationIndex) {
+                                    $(item).addClass("nearestLocation");
+                                    map.panTo(locations[minDurationIndex]);
+                                }
+                            });
+                        }
+                    });
+            }, function () {
+                handleNoGeolocation(browserSupportFlag);
+            });
+        }
+        // Browser doesn't support Geolocation
+        else {
+            browserSupportFlag = false;
+            handleNoGeolocation(browserSupportFlag);
         }
 
-        map.panTo(location);
+        //
+        function handleNoGeolocation(errorFlag) {
+            if (errorFlag == true) {
+                alert("Không thể xác định vị trí hiện tại của bạn.");
+            } else {
+                alert("Trình duyệt của bạn không hỗ trợ định vị hoặc bạn chưa/không cho phép chúng tôi định vị bạn.");
+            }
+            map.panTo(locations[0]);
+        }
     });
-});
+}
+
+function loadScript() {
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = "http://maps.googleapis.com/maps/api/js?key=AIzaSyDYGj0U9_vBLmQ6NhzN7R_T2vBS-Up_UvQ&sensor=true&callback=initialize&v=3";
+    document.body.appendChild(script);
+}
+
+window.onload = loadScript;
